ceb365a243c0292632e70c9b339af941565c8c2d
Hayden Elza <HaydenElza@users.noreply.github.com>
2022-02-28 11:04:10 -0600

    Create expression.py

---
+++ example_pdfs/parcels - madison/expression.py
@@ -0,0 +1,11 @@
+# Paste this into the symbology expression window, changing the colors to your palette
+array_get(
+    array(
+        '184, 216, 216',
+        '122, 158, 159',
+        '79, 99, 103',
+        '238, 245, 219',
+        '254, 95, 85'
+    ),
+    rand(0,4)
+)
